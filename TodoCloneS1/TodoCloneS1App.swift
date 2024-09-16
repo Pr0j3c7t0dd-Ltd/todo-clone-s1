161fa59b7985dfe688493338b486921d38868624
Todd Anderson <todd@pr0j3c7t0dd.com>
2024-09-16 12:52:32 +0100

    initial commit

---
+++ TodoCloneS1/TodoCloneS1App.swift
@@ -0,0 +1,18 @@
+//
+//  TodoCloneS1App.swift
+//  TodoCloneS1
+//
+//  Created by Todd - EE on 16/09/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TodoCloneS1App: App {
+
+    var body: some Scene {
+        WindowGroup {
+        }
+    }
+}
